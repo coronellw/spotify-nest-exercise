d47fe69bc2f12c7fb83b0b51ce7cb1fa24fa068f
Wiston Coronell <coronell@gmail.com>
2025-03-07 19:01:33 -0400

    adding employees and db service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { databaseConfig } from './db/db.config';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [SongsModule, SequelizeModule.forRoot(databaseConfig), UsersModule],
+  imports: [SongsModule, UsersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('songs')
+    consumer.apply(LoggerMiddleware).forRoutes('songs');
     //consumer.apply(LoggerMiddleware).forRoutes({path:'songs', method: RequestMethod.POST})
     // consumer.apply(LoggerMiddleware).forRoutes(SongsController)
   }
--- src/db/db.config.ts
+++
@@ -1,8 +0,0 @@
-import { SequelizeModuleOptions } from "@nestjs/sequelize";
-
-export const databaseConfig: SequelizeModuleOptions = {
-    dialect: 'sqlite',
-    storage: '.db/data.sqlite3',
-    autoLoadModels: true,
-    synchronize: false,
-}
